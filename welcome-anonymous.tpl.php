9e594949076bfe39a1bc390a79b4fc98f23916f4
Derrek Croney <derrek.croney@duke.edu>
2013-06-21 11:31:01 -0400

    initial commit of working files

---
+++ welcome-anonymous.tpl.php
@@ -0,0 +1,5 @@
+<p><img src="/sites/all/themes/dul_master_2013/images/devillogo-60pct-60.png" alt="Reading Devil" title="Reading Devil" width="60" height="105" style="float: right;">This website serves as the place to begin the process of entering content for the new Drupal-powered Duke University Library website. &nbsp;</p>
+
+<p>To log into the website, please click the "Sign on with your NetID" link, located in the <strong>Duke NetID Login</strong> box on the right side of the page.</p>
+
+<p>The content on this website will be migrated to the final, production website in time for the big "launch" in October!</p><p>In the event you experience log-in difficulties, <strong><a href="contact/Website-feedback">please send a note to Derrek Croney</a></strong> and he will provide quick resolution.</p>
---
+++ welcome-loggedin.tpl.php
@@ -0,0 +1,5 @@
+<p><img src="/sites/all/themes/dul_master_2013/images/devillogo-60pct-60.png" alt="Reading Devil" title="Reading Devil" width="60" height="105" style="float: right;">This website serves as the place to begin the process of entering content for the new Drupal-powered Duke University Library website. &nbsp;</p>
+
+<p>You now have the ability to open the Dashboard, using the link located in the masthead area (or simply <a href="/#overlay=admin/dashboard">click here</a>).</p><p>The content on this website will be migrated to the final, production website in time for the big "launch" in October!</p>
+
+<p>In the event you do not have content editing privileges, <strong><a href="contact/Website-feedback">please send a note to Derrek Croney</a></strong> for user provisioning.</p>
