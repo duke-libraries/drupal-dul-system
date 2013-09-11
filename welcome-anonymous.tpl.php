0d7678ec2c0825d079c0382341cc1f0bd793604d
Derrek Croney <derrek.croney@duke.edu>
2013-09-11 10:55:48 -0400

    removed reference to dul_master_2013 theme

--- welcome-anonymous.tpl.php
+++ welcome-anonymous.tpl.php
@@ -1,4 +1,4 @@
-<p><img src="/sites/all/themes/dul_master_2013/images/devillogo-60pct-60.png" alt="Reading Devil" title="Reading Devil" width="60" height="105" style="float: right;">This website serves as the place to begin the process of entering content for the new Drupal-powered Duke University Library website. &nbsp;</p>
+<p><img src="/sites/all/themes/madlib/images/devillogo-60pct-60.png" alt="Reading Devil" title="Reading Devil" width="60" height="105" style="float: right;">This website serves as the place to begin the process of entering content for the new Drupal-powered Duke University Library website. &nbsp;</p>
 
 <p>To log into the website, please click the "Sign on with your NetID" link, located in the <strong>Duke NetID Login</strong> box on the right side of the page.</p>
 
