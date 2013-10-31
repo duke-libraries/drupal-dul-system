9acd99de73e6c6cf97acf612ed0a59c149efd8fe
Derrek Lemar Croney <derrek.croney@duke.edu>
2013-10-31 15:39:04 -0400

    created a template for rendering the twitter feed for datagis

---
+++ twitter-datagis.tpl.php
@@ -0,0 +1,9 @@
+<div class="shortTwitterWidget" id="tweets">
+<div class="header">
+<h4><a href="http://twitter.com/duke_data" target="_blank">Follow Us @duke_data</a></h4>
+</div>
+<iframe frameborder="0" height="270" scrolling="no" src="<?php print base_path(); ?>/twitter/datagis.html" width="100%"></iframe>
+<div class="footer shortFooter">
+<div class="icon"><img src="http://widgets.twimg.com/i/widget-logo.png"></div>
+</div>
+</div>
