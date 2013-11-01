f4b31b224e5e01e2832a09bea667d20741c3ec2a
Derrek Croney <derrek.croney@duke.edu>
2013-11-01 05:20:02 -0400

    added server_error template for use with HTTP 500 errors

---
+++ server_error.tpl.php
@@ -0,0 +1,11 @@
+<p>
+	<a href="http://www.flickr.com/photos/dukeyearlook/"><img src="/sites/default/files/dul/using-the-library/blakely-1.jpg" alt="Photo of librarian helping a student" title="Photo from Duke Yearlook" width="400" height="275" class="caption-right img-polaroid"></a>
+</p>
+<h2>We've redesigned our site &amp; some things have moved!</h2>
+<h3>Let us help:</h3>
+<ul class="large bullet-green spacing-med">
+	<li style="text-align: left;"><a href="/">Visit our new homepage</a></li>
+	<li style="text-align: left;"><a href="/research/ask">Ask a librarian</a> for help finding the page</li>
+	<li style="text-align: left;">Search our site using the box at the top right of your screen</li>
+</ul>
+	
