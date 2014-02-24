7a35a09984365d9f9630b04e676dece6fd0cabe7
Derrek Croney <derrek.croney@duke.edu>
2014-02-24 09:13:10 -0500

    added error response for 410 code

---
+++ server_resource_gone.tpl.php
@@ -0,0 +1,8 @@
+<p>
+	<a href="http://www.flickr.com/photos/dukeyearlook/"><img src="/sites/default/files/dul/using-the-library/blakely-1.jpg" alt="Photo of librarian helping a student" title="Photo from Duke Yearlook" width="400" height="275" class="caption-right img-polaroid"></a>
+</p>
+<p>The resource you requested has been removed from our website and is no longer available.</p>
+
+<p>You can <a href="/find">search</a> for resources using the search box at the top-right of the page.</p>
+	<a href="tel:9196605870">(919) 660-5870</a>.
+</p>
