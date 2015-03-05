7fb54ce9f9d3e65cfb2fa10d44acf94f93a28b8b
Derrek Croney <dlc32@duke.edu>
2015-03-05 13:04:10 -0500

    wrapped original content into a div

--- theme/springbreak-2015-maint.tpl.php
+++ theme/springbreak-2015-maint.tpl.php
@@ -1,6 +1,8 @@
+<div class="alert alert-mild alert-dismissable">
 <h3>Scheduled Outage for Library Website</h3>
 <p>The Duke Libraries website will be down for scheduled maintenance between 9:30 a.m. and 10:30 a.m. on Wednesday, March 11.  
 	During that time, you will be able to go directly to 
 	<a href="http://search.library.duke.edu/#tab1">Books & Media</a> and 
 	<a href="http://duke.summon.serialssolutions.com/#!/search">Articles</a> to search for books, articles, and other resources.
 </p>
+</div>
