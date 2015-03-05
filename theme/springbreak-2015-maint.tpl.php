25539a0e52a55b1e4a10e396382320ce55d2e171
Derrek Croney <dlc32@duke.edu>
2015-03-05 13:01:42 -0500

    changed h2 to h3 for block title

--- theme/springbreak-2015-maint.tpl.php
+++ theme/springbreak-2015-maint.tpl.php
@@ -1,4 +1,4 @@
-<h2>Scheduled Outage for Library Website</h2>
+<h3>Scheduled Outage for Library Website</h3>
 <p>The Duke Libraries website will be down for scheduled maintenance between 9:30 a.m. and 10:30 a.m. on Wednesday, March 11.  
 	During that time, you will be able to go directly to 
 	<a href="http://search.library.duke.edu/#tab1">Books & Media</a> and 
